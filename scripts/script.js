17877c0fd8e78d28656543dc5a315fb3f1ddbb1c
jmontanholi <jonunes9326@hotmail.com.br>
2021-07-05 14:55:35 -0300

    Add remove button and functionalities

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,48 @@ let submit = document.getElementById('submit')
 let title = document.getElementById('title')
 let author = document.getElementById('author')
 let ulbook = document.getElementById('ulBooks')
-
 let booksArray = []
+
+if (localStorage.getItem('booksArray')) {
+    const getBooks = JSON.parse(localStorage.getItem('booksArray'));
+    getBooks.forEach((t) => {
+      booksArray.push(t);
+    });
+}
+
+const saveLocalstorage = () => {
+    localStorage.setItem('booksArray', JSON.stringify(booksArray));
+};
+
+let createList = () => {
+    ulbook.innerHTML = ''
+    for(let i = 0; i < booksArray.length; i++) {
+        let newLi = document.createElement('li')
+        newLi.innerHTML = ` 
+        <p>${booksArray[i].title}</p>
+        <p>${booksArray[i].author}</p>
+        <button id="remove${i}">Remove Book</button>
+        `
+        ulbook.appendChild(newLi)
+    }
+}
+
+createList()
+
+let eventLoop = () => {
+    let removeBtn = document.querySelectorAll('li button')
+    removeBtn.forEach(function(e){
+        e.addEventListener('click', (t) => {
+            let id = t.target.id
+            booksArray.splice((id.slice((id.length -1), id.length)), 1)
+            saveLocalstorage()
+            createList()
+            eventLoop()
+        })
+    })
+}
+
+eventLoop()
 
 class Book {
     constructor(title, author) {
@@ -19,11 +59,6 @@ let addBooks = (book) => {
 submit.addEventListener('click', () => {
     let book = new Book(title.value, author.value)
     addBooks(book)
-    console.log(booksArray)
-    let newLi = document.createElement('li')
-    newLi.innerHTML = ` 
-    <p>${book.title}</p>
-    <p>${book.author}</p>
-    `
-    ulbook.appendChild(newLi)
+    saveLocalstorage()
+    createList()
 })
