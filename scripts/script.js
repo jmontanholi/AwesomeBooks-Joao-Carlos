1ded0272534496abbf785ec186483c9e3b9c8f0d
Carlos Ospina <carlossospinag@gmail.com>
2021-07-05 14:52:36 -0500

    fix ES linters Errors

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,64 @@
-let submit = document.getElementById('submit')
-let title = document.getElementById('title')
-let author = document.getElementById('author')
-let ulbook = document.getElementById('ulBooks')
-let booksArray = []
+const submit = document.getElementById('submit');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const ulbook = document.getElementById('ulBooks');
+const booksArray = [];
 
 if (localStorage.getItem('booksArray')) {
-    const getBooks = JSON.parse(localStorage.getItem('booksArray'));
-    getBooks.forEach((t) => {
-      booksArray.push(t);
-    });
+  const getBooks = JSON.parse(localStorage.getItem('booksArray'));
+  getBooks.forEach((t) => {
+    booksArray.push(t);
+  });
 }
 
 const saveLocalstorage = () => {
-    localStorage.setItem('booksArray', JSON.stringify(booksArray));
+  localStorage.setItem('booksArray', JSON.stringify(booksArray));
 };
 
-let createList = () => {
-    ulbook.innerHTML = ''
-    for(let i = 0; i < booksArray.length; i++) {
-        let newLi = document.createElement('li')
-        newLi.innerHTML = ` 
+const createList = () => {
+  ulbook.innerHTML = '';
+  for (let i = 0; i < booksArray.length; i += 1) {
+    const newLi = document.createElement('li');
+    newLi.innerHTML = ` 
         <p>${booksArray[i].title}</p>
         <p>${booksArray[i].author}</p>
         <button id="remove${i}">Remove Book</button>
-        `
-        ulbook.appendChild(newLi)
-    }
-}
+        `;
+    ulbook.appendChild(newLi);
+  }
+};
 
-createList()
+createList();
 
-let eventLoop = () => {
-    let removeBtn = document.querySelectorAll('li button')
-    removeBtn.forEach(function(e){
-        e.addEventListener('click', (t) => {
-            let id = t.target.id
-            booksArray.splice((id.slice((id.length -1), id.length)), 1)
-            saveLocalstorage()
-            createList()
-            eventLoop()
-        })
-    })
-}
+const eventLoop = () => {
+  const removeBtn = document.querySelectorAll('li button');
+  removeBtn.forEach((e) => {
+    e.addEventListener('click', (t) => {
+      const { id } = t.target;
+      booksArray.splice((id.slice((id.length - 1), id.length)), 1);
+      saveLocalstorage();
+      createList();
+      eventLoop();
+    });
+  });
+};
 
-eventLoop()
+eventLoop();
 
 class Book {
-    constructor(title, author) {
-        this.title = title;
-        this.author = author;
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
-let addBooks = (book) => {
-    booksArray.push(book)
-} 
+const addBooks = (book) => {
+  booksArray.push(book);
+};
 
 submit.addEventListener('click', () => {
-    let book = new Book(title.value, author.value)
-    addBooks(book)
-    saveLocalstorage()
-    createList()
-})
+  const book = new Book(title.value, author.value);
+  addBooks(book);
+  saveLocalstorage();
+  createList();
+});
