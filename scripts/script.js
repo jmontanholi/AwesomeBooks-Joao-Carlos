b6fa15c97179f805d4e0a8c01a6c0c44097f5c26
carloso1401 <carlossospinag@gmail.com>
2021-07-05 11:48:35 -0500

    add Books function, display books in li

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,29 @@
 let submit = document.getElementById('submit')
 let title = document.getElementById('title')
 let author = document.getElementById('author')
+let ulbook = document.getElementById('ulBooks')
 
-submit.addEventListener()
+let booksArray = []
+
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+
+let addBooks = (book) => {
+    booksArray.push(book)
+} 
+
+submit.addEventListener('click', () => {
+    let book = new Book(title.value, author.value)
+    addBooks(book)
+    console.log(booksArray)
+    let newLi = document.createElement('li')
+    newLi.innerHTML = ` 
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    `
+    ulbook.appendChild(newLi)
+})
